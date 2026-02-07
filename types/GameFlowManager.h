aa21e3e2fd5ec11efd086706c3ab2227e3f8ee93
💖 Chloe 💖 <gaycoderprincess@proton.me>
2026-02-07 18:58:52 +0100

    GameFlowManager::ReloadTrack

--- types/GameFlowManager.h
+++ types/GameFlowManager.h
@@ -13,6 +13,7 @@ public:
 	static inline auto UnloadTrack = (void(__thiscall*)(GameFlowManager*))0x6DBC60;
 	static inline auto LoadFrontend = (void(__thiscall*)(GameFlowManager*))0x6D3230;
 	static inline auto UnloadFrontend = (void(__thiscall*)(GameFlowManager*))0x6D3290;
+	static inline auto ReloadTrack = (void(__thiscall*)(GameFlowManager*))0x6DBC10;
 };
 static_assert(sizeof(GameFlowManager) == 0x24);
 
